7ec341ba04fe44c3f12c77d24fe29b9e4ce3c187
TuHechiceroFavorito <daniel.alvarez.car@gmail.com>
2021-07-09 17:26:59 +0200

    Log out

--- heroweb/heroweb/urls.py
+++ heroweb/heroweb/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('create_account/', register, name="create_account"),
     #That is an automatic view done by Django. I'm only defining the file that it has to redirect to
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
+    # Same as login but for loggin out
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
 ]
