43bcdfb3628d051576ba57134d17b23aba5154f8
TuHechiceroFavorito <daniel.alvarez.car@gmail.com>
2021-07-09 18:03:10 +0200

    Password reset infrastructure setted. Left to add the email and credentials

--- heroweb/heroweb/urls.py
+++ heroweb/heroweb/urls.py
@@ -21,12 +21,34 @@ from users.views import register, mock_home, profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     #This is just a mock version of a home page for me to test the redirection functionalities
     path('mock_home/', mock_home, name="home"),
     path('create_account/', register, name="create_account"),
+
     #That is an automatic view done by Django. I'm only defining the file that it has to redirect to
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
+
     # Same as login but for loggin out
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
     path('profile/', profile, name="profile"),
+    
+    # PASSWORD RESET
+    # Automatic view for password reset
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='pass_reset/password_reset.html'),
+        name="password_reset"),
+    # Automatic view for password done sending the email
+    path('password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(template_name='pass_reset/password_reset_done.html'),
+        name="password_reset_done"),
+    #Confirmation of password reset. This is the page that the link emailed takes to
+    # The <> things are placeholders. They are the key and the token that Django provides for security. It's
+    # automatically done by the view
+    path('password_reset_confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name='pass_reset/password_reset_confirm.html'),
+        name="password_reset_confirm"),
+    # Reset completed page
+    path('password_reset_complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name='pass_reset/password_reset_complete.html'),
+        name="password_reset_complete"),
 ]
