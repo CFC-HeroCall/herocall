ffa91da7516b9277491dfe97c55a8471edbc6999
TuHechiceroFavorito <daniel.alvarez.car@gmail.com>
2021-07-23 13:49:19 +0200

    Added simple posts objects

--- heroweb/main/models.py
+++ heroweb/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    text = models.CharField(max_length=2000)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.author.username} --> {self.title}'
+
+class Tab(models.Model):
+    title = models.CharField(max_length=120)
+    text = models.CharField(max_length=2000)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.post.title} --> {self.title}'
--- heroweb/main/views.py
+++ heroweb/main/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from main.models import Post, Tab
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
 
 def explore(request):
-    return render(request, 'explore.html')
+    post = Post.objects.all()[0]
+    tabs = Tab.objects.all().filter(post=post)
+    content = {
+        'post':post,
+        'tabs':tabs
+    }
+    return render(request, 'explore.html', content)
