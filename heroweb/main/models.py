608c9360c9a6a090c46b0ff4dcc34ee5402fd696
TuHechiceroFavorito <daniel.alvarez.car@gmail.com>
2021-07-23 15:09:05 +0200

    Showing replies

--- heroweb/main/models.py
+++ heroweb/main/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=120)
     text = models.CharField(max_length=2000)
     author = models.ForeignKey(User, on_delete=models.PROTECT)
+    reply = models.IntegerField(null=True)
 
     def __str__(self):
         return f'{self.author.username} --> {self.title}'
--- heroweb/main/views.py
+++ heroweb/main/views.py
@@ -7,9 +7,21 @@ def home(request):
 
 def explore(request):
     post = Post.objects.all()[0]
+    replies_raw = Post.objects.all().filter(reply=post.id)
+    views = []
+    i = 0
+    for reply in replies_raw:
+        if i % 2 == 0:
+            views.append([reply])
+        else:
+            views[len(views)-1].append(reply)
+        i += 1
+
+    print(views)
     tabs = Tab.objects.all().filter(post=post)
     content = {
         'post':post,
-        'tabs':tabs
+        'tabs':tabs,
+        'views':views
     }
     return render(request, 'explore.html', content)
